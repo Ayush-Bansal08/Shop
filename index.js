395bfb1a3ac3a32d1d100ff08102d9a42c1233a6
Ayush Bansal <ab262133@gmail.com>
2024-10-20 12:38:01 +0530

    new pages added

--- index.js
+++ index.js
@@ -3,128 +3,128 @@ const products = [
         name: 'Louis Vuitton Bag',
         path: 'Images/Luivitton.png.jpg',
         price: '$2500',
-        memes: [{ text: 'Luxury on the Go', audioPathh: 'audio/AukatMeRahoMeme.mp4' }]
+        memes: [{ text: 'Great for showing off your style or for hiding all your dirty little secrets!', audioPathh: './audio/AukatMeRahoMeme.mp4' }]
     },
     {
         name: 'Air Jordan',
         path: 'Images/shoes.png.jpg',
-        price: '$920',
-        memes: [{ text: 'Step in Style', audioPathh: 'audio/80shoes.mp4' }]
+        price: '$1920',
+        memes: [{ text: ' Built for flying high on the court or for elevating your game when things get steamy!', audioPathh: './audio/80shoes.mp4' }]
     },
     {
         name: 'Rocking Horse',
         path: 'Images/dogdonkey.jpg.jpg',
-        price: '$50000',
-        memes: [{ text: 'Animal Adventures', audioPathh: 'audio/donkeydog.mp4' }]
+        price: '$49999',
+        memes: [{ text: 'A masterpiece ? More like a mess-terpiece', audioPathh: './audio/donkeydog.mp4' }]
     },  
     {
         name: 'Hairband',
         path: 'Images/hairband.png',
-        price: '$6',
-        memes: [{ text: 'Tie it up!', audioPathh: 'audio/miniskirt.mp4' }]
+        price: '$89',
+        memes: [{ text: 'Great for keeping your hair in place or for holding on tight when things get messy!', audioPathh: './audio/miniskirt.mp4' }]
     },
     {
-        name: 'iPhone',
+        name: 'IPhone 16 pro max',
         path: 'Images/iphone.png.jpg',
         price: '$999',
-        memes: [{ text: 'Next-Gen Tech', audioPathh: 'audio/AukatMeRahoMeme.mp4' }]
+        memes: [{ text: 'Because size matters... especially when its all in your hands !', audioPathh: './audio/AukatMeRahoMeme.mp4' }]
     },
     {
         name: 'Banana',
         path: 'Images/banana.png',
         price: '$10',
-        memes: [{ text: 'Tropical delight', audioPathh: 'audio/Banana,cucumber,eggplant.mp4' }]
+        memes: [{ text: ' Great for a quick snack or for when you want to slip into a good time!', audioPathh: './audio/Banana,cucumber,eggplant.mp4' }]
     },
     // {
     //     name: 'Cucumber',
     //     path: 'Images/cucumber.png',
     //     price: '$15',
-    //     memes: [{ text: 'Cool as a cucumber', audioPathh: 'audio/Banana,cucumber,eggplant.mp4' }]
+    //     memes: [{ text: 'Cool as a cucumber', audioPathh: './audio/Banana,cucumber,eggplant.mp4' }]
     // },
     {
         name: 'Danda',
         path: 'Images/danda.jpg',
-        price: '$8',
-        memes: [{ text: 'Stay in line', audioPathh: 'audio/spank,danda.mp4' }]
+        price: '$18',
+        memes: [{ text: 'Stay in line', audioPathh: './audio/spank,danda.mp4' }]
     },
     // {
     //     name: 'Eggplant',
     //     path: 'Images/eggplant.png',
     //     price: '$12',
-    //     memes: [{ text: 'Egg-cellent choice!', audioPathh: 'audio/Banana,cucumber,eggplant.mp4' }]
+    //     memes: [{ text: 'Egg-cellent choice!', audioPathh: './audio/Banana,cucumber,eggplant.mp4' }]
     // },
     // {
     //     name: 'Handcuffs',
     //     path: 'Images/handcuffs.png',
     //     price: '$20',
-    //     memes: [{ text: 'Locked and loaded', audioPathh: 'audio/handcuff.mp4' }]
+    //     memes: [{ text: 'Locked and loaded', audioPathh: './audio/handcuff.mp4' }]
     // },
     {
         name: 'Heart Cushion',
         path: 'Images/heartCushion.png',
-        price: '$18',
-        memes: [{ text: 'Soft and sweet', audioPathh: 'audio/purse.mp4' }]
+        price: '$38',
+        memes: [{ text: 'Perfect for a cozy embrace or for getting a little too close during those late-night chats!', audioPathh: './audio/purse.mp4' }]
     },
     {
-        name: 'Lauger',
+        name: 'Lounge Chair',
         path: 'Images/lauger.png',
-        price: '$25',
-        memes: [{ text: 'Chill vibes', audioPathh: 'audio/last.mp4' }]
+        price: '$249',
+        memes: [{ text: 'Great for kicking back and relaxing... or for getting cozy when things heat up!', audioPathh: './audio/last.mp4' }]
     },
     {
-        name: 'Melon',
+        name: 'Mast Melon',
         path: 'Images/melon.png',
-        price: '$10',
-        memes: [{ text: 'Melon madness!', audioPathh: 'audio/melon1,2.mp4' }]
+        price: '$19',
+        memes: [{ text: 'Juicy!', audioPathh: './audio/melon1,2.mp4' }]
     },
     {
         name: 'Miniskirt',
         path: 'Images/miniskirt.png',
-        price: '$22',
-        memes: [{ text: 'Stylish choice!', audioPathh: 'audio/miniskirt.mp4' }]
+        price: '$169',
+        memes: [{ text: ' Short enough to keep things interesting but long enough to stay classy!', audioPathh: './audio/miniskirt.mp4' }]
     },
     {
-        name: 'Spank',
+        name: 'Baton',
         path: 'Images/spank.png',
-        price: '$30',
-        memes: [{ text: 'Playful vibes', audioPathh: 'audio/spank,danda.mp4' }]
+        price: '$69',
+        memes: [{ text: 'To keep in right track', audioPathh: './audio/spank,danda.mp4' }]
     },
     {
         name: 'Teddy',
         path: 'Images/teddy.png',
-        price: '$35',
-        memes: [{ text: 'Cuddle buddy', audioPathh: 'audio/teddy.mp4' }]
+        price: '$85',
+        memes: [{ text: 'Always down for a cuddle,No questions asked', audioPathh: './audio/teddy.mp4' }]
     },
     {
         name: 'Toilet Paper',
         path: 'Images/toiletpaper.png',
-        price: '$3',
-        memes: [{ text: 'Emergency essential', audioPathh: 'audio/last.mp4' }]
+        price: '$29',
+        memes: [{ text: 'Even the toughest problem need a Roll model', audioPathh: './audio/last.mp4' }]
     },
     {
         name: 'Vaseline',
         path: 'Images/vaseline.png',
-        price: '$5',
-        memes: [{ text: 'Stay moisturized', audioPathh: 'audio/purse.mp4' }]
+        price: '$13',
+        memes: [{ text: ' Smooth moves, anytime you need a little extra glide!', audioPathh: './audio/purse.mp4' }]
     },
     {
-        name: 'iPad',
+        name: 'IPad',
         path: 'Images/ipad.jpg.jpg',
         price: '$799',
-        memes: [{ text: 'Powerful and Portable', audioPathh: 'audio/AukatMeRahoMeme.mp4' }]
+        memes: [{ text: 'Bigger, better and always ready to be touched', audioPathh: './audio/AukatMeRahoMeme.mp4' }]
     },
     
     {
-        name: 'MacBook',
+        name: 'MacBook M2',
         path: 'Images/Macbook.png.jpg',
         price: '$1299',
-        memes: [{ text: 'Performance Unleashed', audioPathh: 'audio/AukatMeRahoMeme.mp4' }]
+        memes: [{ text: 'Handles more tabs than you can, and still never slows down!', audioPathh: './audio/AukatMeRahoMeme.mp4' }]
     },
     {
-        name: 'Melon 2',
+        name: 'Musk Melon ',
         path: 'Images/melon2.png',
         price: '$12',
-        memes: [{ text: 'Double the fun', audioPathh: 'audio/melon1,2.mp4' }]
+        memes: [{ text: ' Sweet, juicy, and always ready to be handled with care!', audioPathh: './audio/melon1,2.mp4' }]
     },
 ];
 
